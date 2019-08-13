f76c44db695298f3aa1e8877cb60e5c12c7ea4a5
Shubham Gawande <ishubhamgawande@gmail.com>
2019-08-13 17:56:06 +0530

    overhaul with animation and quantix case study

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+    //Wow JS integration
+    new WOW().init();
+
+    //Mobile Menu Toggle Function
+    $('#nav').click(function(){
+        $('.navWrapper').toggle();
+    })
+    
+    });
