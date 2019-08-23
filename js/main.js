9c22f996301fa32aae3f089e5b596737e4c086e2
Shubham Gawande <ishubhamgawande@gmail.com>
2019-08-24 00:38:38 +0530

    Google Analytics + Mobile Menu

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@ $(document).ready(function(){
     //Mobile Menu Toggle Function
     $('#nav').click(function(){
         $('.navWrapper').toggle();
+        $('.navWrapper').addClass('wow');
+        $('.navWrapper').addClass('animated');
+        $('.navWrapper').addClass('fadeInUpBig');
+        $('.navWrapper').addClass('fast');
         $('.navWrapper ul li').addClass('wow');
         $('.navWrapper ul li').addClass('animated');
         $('.navWrapper ul li').addClass('fadeInUp');
+        $('.navWrapper ul li').addClass('delay-1s');
     })
     
     });
